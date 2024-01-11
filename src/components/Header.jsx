6bc72f2d586463c01e200e3f1f7486f878b1e166
Avisenna Abimanyu <theknightmare07@gmail.com>
2024-01-11 22:34:59 +0700

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import logo from '../assets/image-1.svg';
+// import logo from '../assets/image-1.svg';
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
-      <img
+    <header className="flex justify-between items-center pt-2 pb-2 pd:pb-16 px-10 bg-blue-200">
+      {/* <img
         src={logo}
         alt="A canvas"
         className="object-contain mb-8 w-44 h-44"
@@ -11,7 +11,14 @@ export default function Header() {
       <h1 className="text-xl font-semibold tracking-widest text-center uppercase md:text-4xl text-amber-800 font-title">
         ReactArt
       </h1>
-      <p className="text-stone-500">A community of artists and art-lovers.</p>
+      <p className="text-stone-500">A community of artists and art-lovers.</p> */}
+      <span>HOME</span>
+      <div className="flex justify-around items-center">
+        <span className="mx-2 font-bold">ABOUT</span>
+        <span className="mx-2 font-bold">PRICING</span>
+        <span className="mx-2 font-bold">CONTACT</span>
+        <button className="mx-2 text-xs font-bold text-cyan-400 border-solid border-2 border-sky-500 rounded p-2">LOGIN</button>
+      </div>
     </header>
   );
 }
